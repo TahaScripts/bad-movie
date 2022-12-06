f222174e4cad7eb017ab42a7ca1676a4c315634c
Taha Mohamad <98236583+TahaScripts@users.noreply.github.com>
2022-12-06 11:51:18 -0500

    added env variable for key

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,11 +1,11 @@
 export async function getCategoryPoster(category) {
-    const details = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2&with_genres=' + category + '&with_watch_monetization_types=flatrate')
+    const details = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=' + process.env.MOVIE_KEY + '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2&with_genres=' + category + '&with_watch_monetization_types=flatrate')
     const data = await details.json();
     return 'https://image.tmdb.org/t/p/w185' + data.results[0].poster_path
 }
 
 export async function getCategories() {
-    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&language=en-US')
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + process.env.MOVIE_KEY + '&language=en-US')
     const data = await res.json();
 
     let newData = await Promise.all(data.genres.map(async (i) => {return [i.id, i.name, await getCategoryPoster(i)]}))
